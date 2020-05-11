fbd1653dfbec235d78583ae7a8f64dc121d189a0
alejandro <eam.rodriguez@gmail.com>
2020-05-11 19:04:32 -0400

    agregamos ProyectoCedulaBeta __init__ settings urls wsgi asgi

---
+++ ProyectoCedulaBeta/urls.py
@@ -0,0 +1,31 @@
+"""ProyectoCedulaBeta URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import path
+from ProyectoCedulaBetaApp import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', views.index),
+    path('buscar/', views.buscar),
+    path('', views.busqueda_cedula),
+    path('contacto/', views.contacto),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
